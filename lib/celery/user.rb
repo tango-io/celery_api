daf15c3b0653b8c070b2baeff672df7f0d8d2dd4
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2014-06-17 22:40:28 -0500

    Now retrieved user

--- lib/celery/user.rb
+++ lib/celery/user.rb
@@ -9,6 +9,16 @@ module Celery
       :subscription, :business, :has_affirm, :message_to_buyer, :access_token,
       :emails, :has_paypalx
 
+
+    class << self
+      def me
+        query_string  = Celery.endpoint + "users/me?"
+        query_string += Celery.parameterize_options
+        response = HTTParty.get(query_string)
+
+        self.new(response['data'])
+      end
+    end
   end
 
 end
--- spec/celery/user_spec.rb
+++ spec/celery/user_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
-describe Celery::User do
+describe Celery::User, 'class methods' do
+
+  it 'gets the user' do
+    expect(Celery::User.me).to be_kind_of Celery::User
+  end
+
+end
+
+describe Celery::User, 'instance methods' do
   let!(:attrs) { { "email" => Faker::Internet.email, "name" => Faker::Name.name } }
   let!(:user) { Celery::User.new(attrs) }
 
